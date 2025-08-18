96bb314f3aea4c7c0f5345c4fd18a2acefa286fa
SUHYUN YU <suhyun1219yu@gmail.com>
2025-08-18 21:48:18 +0900

    깃이그노어 추가, env파일 추가

--- src/main/java/com/springboot/rerise/ReriseApplication.java
+++ src/main/java/com/springboot/rerise/ReriseApplication.java
@@ -1,5 +1,6 @@
 package com.springboot.rerise;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReriseApplication {
 
     public static void main(String[] args) {
+        // 1. .env 파일 로드
+        Dotenv dotenv = Dotenv.load();
+
+        // 2. 환경변수 등록
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
+        // 3. 스프링 부트 실행
         SpringApplication.run(ReriseApplication.class, args);
     }
 
